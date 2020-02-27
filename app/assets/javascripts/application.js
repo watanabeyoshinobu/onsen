00c770e1f85ce8396c3e71ffab62baaeba751230
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2020-02-27 06:45:02 +0000

    デプロイ用

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,7 +48,15 @@ $(function() {
   });
 
 
-
-
 });
 
+$(function() {
+
+  $('#back a').on('click',function(){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+      return false;
+  });
+
+});
