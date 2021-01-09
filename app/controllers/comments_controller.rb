aa05f917d3e414cf4cbb3e1f4ea22caa86ef4da7
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-01-09 07:53:52 +0000

    comment update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,15 @@ class CommentsController < ApplicationController
 	def create
 	    comment = Comment.new(comment_params)
     		@comment.user_id = current_user.id
-	      if comment.save
+	    if comment.save
 	        flash[:notice] = 'コメントを投稿しました！'
 	        redirect_to '/blogs'
-	      else
+	    else
 	        redirect_to :back, flash: {
 	            comment: comment,
 	            error_messages: comment.errors.full_messages
 	        }
-      end
+      	end
 	end
 
 	def destroy
