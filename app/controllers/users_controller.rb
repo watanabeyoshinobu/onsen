201e84e58ed1d7672b8884218ecf8772d2a96ca7
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-02-07 01:37:19 +0000

    users update and README uodate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 
   def update
+  	@user.update(user_params)
+    flash[:notice] = "「#{@user.name}」の登録を更新しました!"
+    redirect_to users_show_path
   end
 
   def withdraw
