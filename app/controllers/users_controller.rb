2a1c5b5d9ceeb0c1d0c10275ac0adb114ec2eb27
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-03-04 06:22:21 +0000

    users update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
       flash[:success] = "「#{@user.name}」を変更しました！"
       redirect_to users_show_path
     else
