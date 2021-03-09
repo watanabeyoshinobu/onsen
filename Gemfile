d0b4729e612ac0ba3586f7b7e32bcd14071eb15f
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-03-09 01:25:01 +0000

    users update

--- Gemfile
+++ Gemfile
@@ -61,28 +61,18 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
 gem 'devise'
-
 gem 'bootstrap-sass', '~> 3.3.6'
-
 gem 'jquery-rails'
-
 gem 'rails-i18n'
-
 gem 'dotenv-rails'
-
 gem 'kaminari','~> 1.2.1'
-
 gem 'kaminari-bootstrap', '~> 3.0.1'
-
 gem 'pry-byebug'
-
 gem 'annotate'
-
 gem 'sass-rails'
-
 gem 'carrierwave'
+gem 'rmagick'
 
 group :production do
   gem 'mysql2'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,4 +44,42 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  include CarrierWave::RMagick
+
+  process :resize_to_limit => [640, 480]
+
+  process :convert => 'jpg'
+
+  version :thumb do
+    process :resize_to_limit => [300, 300]
+  end
+
+  version :thumb100 do
+    process :resize_to_limit => [100, 100]
+  end
+
+  version :thumb30 do
+    process :resize_to_limit => [30, 30]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
+
 end
