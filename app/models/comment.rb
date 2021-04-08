0d6f4f8354f8f9356913dc605832860284e9e9a4
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-04-08 08:11:28 +0000

    blogs fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog, optional: true
 
-  validates :comment, presence: true, length: { minimum: 3, maximum: 1000 }
+  validates :comment, presence: true, length: { maximum: 1000 }
 
 end
