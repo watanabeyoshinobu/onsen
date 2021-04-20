80f6c1508221218187c9e638f5411c480bc92eba
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-04-20 01:49:05 +0000

    blogs update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -192,3 +192,67 @@ $(function(){
 $(window).on('load', function(){
   $('.onsen-visual6').addClass('is-visible');
 });
+
+
+// check画面
+$(function(){
+  $('.onsen-visual7 .line').children().addBack().contents().each(function(){
+    if (this.nodeType == 3) {
+      var $this = $(this);
+      $this.replaceWith($this.text().replace(/(\S)/g, '<span class="letter">$&</span>'));
+    }
+  });
+
+  $('.onsen-visual7 .letter').each(function () {
+    var letters = $(this).closest('.text').find('.letter');
+    var index = $(letters).index(this);
+    var time = index * 0.09;
+    $(this).css('animation-delay', time + 's');
+  });
+});
+
+$(window).on('load', function(){
+  $('.onsen-visual7').addClass('is-visible');
+});
+
+// try画面
+$(function(){
+  $('.onsen-visual8 .line').children().addBack().contents().each(function(){
+    if (this.nodeType == 3) {
+      var $this = $(this);
+      $this.replaceWith($this.text().replace(/(\S)/g, '<span class="letter">$&</span>'));
+    }
+  });
+
+  $('.onsen-visual8 .letter').each(function () {
+    var letters = $(this).closest('.text').find('.letter');
+    var index = $(letters).index(this);
+    var time = index * 0.09;
+    $(this).css('animation-delay', time + 's');
+  });
+});
+
+$(window).on('load', function(){
+  $('.onsen-visual8').addClass('is-visible');
+});
+
+// gaze画面
+$(function(){
+  $('.onsen-visual9 .line').children().addBack().contents().each(function(){
+    if (this.nodeType == 3) {
+      var $this = $(this);
+      $this.replaceWith($this.text().replace(/(\S)/g, '<span class="letter">$&</span>'));
+    }
+  });
+
+  $('.onsen-visual9 .letter').each(function () {
+    var letters = $(this).closest('.text').find('.letter');
+    var index = $(letters).index(this);
+    var time = index * 0.09;
+    $(this).css('animation-delay', time + 's');
+  });
+});
+
+$(window).on('load', function(){
+  $('.onsen-visual9').addClass('is-visible');
+});
