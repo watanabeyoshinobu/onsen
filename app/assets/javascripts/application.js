f37f54be0c0d0ae085b9a42311895809c577222e
watanabeyoshinobu <nobu.regulus4946@gmail.com>
2021-07-11 06:19:12 +0000

    blogs update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -282,30 +282,6 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  const section = document.querySelector('.sakura2_container');
-
-  const createPetal = () => {
-    const petalEl = document.createElement('span');
-    petalEl.className = 'petal2';
-    const minSize = 10;
-    const maxSize = 20;
-    const size = Math.random() * (maxSize + 1 - minSize) + minSize;
-    petalEl.style.width = `${size}px`;
-    petalEl.style.height = `${size}px`;
-    petalEl.style.left = Math.random() * innerWidth + 'px';
-    section.appendChild(petalEl);
-    setTimeout(() => {
-      petalEl.remove();
-    }, 15000);
-  }
-
-  setInterval(createPetal, 300);
-});
-
-
-
 
 
 
