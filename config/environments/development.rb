290dc7e8f01cf0498325ec0dceaf5b9ebc1c903e
渡邊義信 <yoshinobu@watanabeyoshinobunoMacBook-Pro.local>
2021-09-09 17:29:33 +0900

    js update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::FileUpdateChecker
-  # config.logger = Logger.new(STDOUT)
-  # config.level = 0
+  logger = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
 end
