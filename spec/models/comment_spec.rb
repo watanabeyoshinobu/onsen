27a89656ddff3383327d847d978e5591deb4f305
渡邊義信 <yoshinobu@watanabeyoshinobunoMacBook-Pro.local>
2021-10-02 16:39:51 +0900

    RSpec edit

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  before do
+    @user = create(:user)
+    @post = create(:post)
+    @comment = build(:comment, user_id: @user.id, post_id: @post.id)
+  end
+
+  describe 'バリデーション' do
+    it '内容がない場合、無効である' do
+      @comment.content = nil
+      @comment.valid?
+      expect(@comment.errors[:content]).to include('を入力してください')
+    end
+
+    it '内容が1文字以上、100文字以下の場合、有効である' do
+      expect(@comment).to be_valid
+    end
+
+  end
+end
