a5f42282feb744b82be8e9ac007ad024b35682e7
渡邊義信 <yoshinobu@watanabeyoshinobunoMacBook-Pro.local>
2021-12-09 15:11:50 +0900

    all update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,8 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: %i[update destroy]
 
 
-
-	  protected
+		protected
 
 	  def update_resource(resource, params)
 	    resource.update_without_current_password(params)
