a1d782b846b10770b7bb8edaa39f1bcc5736f7da
渡邊義信 <yoshinobu@watanabeyoshinobunoMacBook-Pro.local>
2022-03-15 23:44:16 +0900

    carrierwave.rb S3 画像投稿するための編集 README 誤字・脱字の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,20 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+  if Rails.env.production? || Rails.env.development?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'onsen-image'
     config.asset_host = 'https://s3.amazonaws.com/onsen-image'
-    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
-    config.fog_public = false # ←コレ
+    config.fog_public = false
     config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: 'AKIAUAG2DQKKV6PMPKI3',
         aws_secret_access_key: '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',
         region: 'ap-northeast-1'
       }
-    config.cache_storage = :fog
+    #config.cache_storage = :fog
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
