ec236a4e4116f3aa17483b26c2d4834c8fa0b10c
渡邊義信 <yoshinobu@watanabeyoshinobunoMacBook-Pro.local>
2022-03-16 17:22:54 +0900

    S3 バグ対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,27 @@
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
+
+# CarrierWave.configure do |config|
+#   if Rails.env.production? || Rails.env.development?
+#     config.storage :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_directory  = 'onsen-image'
+#     config.asset_host = 'https://s3.amazonaws.com/onsen-image'
+#     config.fog_public = false
+#     config.fog_credentials = {
+#         provider: 'AWS',
+#         aws_access_key_id: 'AKIAUAG2DQKKV6PMPKI3',
+#         aws_secret_access_key: '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',
+#         region: 'ap-northeast-1'
+#       }
+#     #config.cache_storage = :fog
+#   else
+#     config.storage :file
+#     config.enable_processing = false if Rails.env.test?
+#   end
+# end
+
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
@@ -5,17 +29,16 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.development?
     config.storage :fog
+    config.cache_storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'onsen-image'
-    config.asset_host = 'https://s3.amazonaws.com/onsen-image'
+    config.fog_directory  = 'onsen-image2'
     config.fog_public = false
     config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: 'AKIAUAG2DQKKV6PMPKI3',
-        aws_secret_access_key: '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',
+        aws_access_key_id: ENV['S3_ACCESS_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET_KEY'],
         region: 'ap-northeast-1'
       }
-    #config.cache_storage = :fog
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
