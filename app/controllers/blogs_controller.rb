72d8a690f7df56a796959e775b376d34ada302e3
渡邊義信 <yoshinobu@watanabeyoshinobunoMacBook-Pro.local>
2024-01-12 19:04:46 +0900

    views/comments/_comments.html.erb 修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,10 +7,9 @@ class BlogsController < ApplicationController
   end
 
   def show
-    # @comment = Comment.new(blog_id: @blog.id)
+    @comment = Comment.new(blog_id: @blog.id)
     @blog = Blog.includes(:user).find(params[:id])
     @comments = @blog.comments
-    @comment = Comment.new
     @tweets = @blog.tweets
     @tweet = Tweet.new
   end
