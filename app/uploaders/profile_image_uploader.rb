bf69d12d85380600f03eb2749110fdac89794b54
渡邊義信 <yoshinobu@MacBookPro.kanax1.kt.home.ne.jp>
2026-01-13 14:50:34 +0900

    ゲストユーザーの画像エラー 修正

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -15,13 +15,10 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "img/no_image.jpg"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "img/no_image.jpg"].compact.join('_')
-  # end
+
+  def default_url(*args)
+    "img/no_image.jpg"
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
